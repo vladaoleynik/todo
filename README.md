a3714929e72a0d67d4554fc1f8effc3502a16621
Vlada Oleynik <voleynik3221@gmail.com>
2015-04-27 22:53:03 +0300

    Adding comments

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # redis
+
+-i task_list_name -a args
+    adding task to the task_list_name list
+
+-i task_list_name -e args
+    editing task in the task_list_name list
+
+-i task_list_name -d all
+    deleting all the tasks from task_list_name list
+
+-i task_list_name -d task_id
+    deleting all the task_id task from task_list_name list
+
+-d all
+    deleting all the task lists in db
+
+-p
+    printing names of all existing in db task lists
+
+-i task_list_name -p
+    printing all the tasks from task_list_name list
+
+
+
--- main.py
+++ main.py
@@ -9,14 +9,19 @@ import logging
 
 class List(argparse.Action):
 
+    # log configuration
+    # all the errors are to be found in mylog.log
     logging.basicConfig(format='%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
                         level=logging.DEBUG, filename='mylog.log')
 
     def __call__(self, parser, namespace, values, option_string):
         self.tasks = dict()
+        # connecting to Redis
         self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        # dumb manipulations with tasks list name
         setattr(namespace, self.dest, values)
         name = None
+        # choosing a method depending on args
         if namespace.init is not None:
             name = str(namespace.init[0])
         if option_string == '-a':
@@ -37,12 +42,15 @@ class List(argparse.Action):
 
     def delete(self, name, task_id):
         if task_id == 'all' and name is None:
+            # db clear
             self.r.flushdb()
             logging.info("All the database was successfully cleared")
         elif name and task_id == 'all':
+            # deleting a single task list
             self.r.delete(name)
             logging.info("Task list was successfully deleted")
         else:
+            # deleting a single task from concrete list
             tasks = self.pull_from_redis(name)
             del tasks[str(task_id)]
             self.push_to_redis(name, tasks)
@@ -56,6 +64,7 @@ class List(argparse.Action):
 
     def print_all(self, name):
         if name is None:
+            # printing names of all the task lists in db
             tasks = self.r.keys()
             if tasks:
                 print("Here are the names of all the task lists:")
@@ -64,6 +73,7 @@ class List(argparse.Action):
             else:
                 logging.warning('Oops..smth went wrong. List is empty')
         else:
+            # printing all the tasks in the task list
             tasks = self.pull_from_redis(name, False)
             if tasks and tasks is not None:
                 for key, value in tasks.iteritems():
@@ -73,10 +83,12 @@ class List(argparse.Action):
             else:
                 logging.warning('Oops..smth went wrong. List is empty')
 
+    # pushing in db
     def push_to_redis(self, name, tasks):
         tasks_json = self.encode_json(tasks)
         self.r.set(name, tasks_json)
 
+    # pulling from db
     def pull_from_redis(self, name, flag=True):
         # flag - to decide whether we should raise an exception
         tasks = self.r.get(name)
@@ -90,14 +102,17 @@ class List(argparse.Action):
                 tasks = dict()
                 return tasks
 
+    # json encode
     def encode_json(self, task):
         return json.dumps(task, sort_keys=True, separators=(',', ': '))
 
+    # json decode
     def decode_json(self, task):
         return json.loads(task)
 
 
 if __name__ == '__main__':
+    # more info about how to use all this stuff - in README.md
     parser = argparse.ArgumentParser(description='Work with Redis database.')
     parser.add_argument('-i', '--init', action=List, nargs=1,
                         help='Initiating a new list')
