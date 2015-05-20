c21eb1308586f5b0bf0dd75609a8cb724c4a468a
Vlada Oleynik <voleynik3221@gmail.com>
2015-05-20 16:48:22 +0300

    Edit task func

--- src/rest.py
+++ src/rest.py
@@ -8,6 +8,16 @@ from bottle import route, run, request
 redis = List.connection()
 
 
+@route('/todo/tasks', method='GET')
+def tasks_list():
+    return List.print_all(redis, None)
+
+
+@route('/todo/tasks/<name>', method='GET')
+def get_task(name):
+    return List.print_all(redis, name)
+
+
 @route('/todo/tasks/<name>', method='POST')
 def add_tasks(name):
     description = request.forms.get('description')
@@ -20,21 +30,15 @@ def add_tasks(name):
             return '201 Create\n'
 
 
-@route('/todo/tasks', method='GET')
-def tasks_list():
-    if List.print_all(redis, None) is None:
-        return 'Nothing to print'
+@route('/todo/tasks/<name>/<task_id:int>', method='PUT')
+def edit_tasks(name, task_id):
+    description = request.forms.get('description')
+    deadline = request.forms.get('deadline')
+    importance = request.forms.get('importance')
+    if List.edit(redis, name, task_id, description, importance, deadline):
+        return '500 Server error\n'
     else:
-        return List.print_all(redis, None)
-
-
-@route('/todo/tasks/<name>')
-def get_task(name):
-    if List.print_all(redis, name) is None:
-        return 'Nothing to print'
-    else:
-        return List.print_all(redis, name)
-    return '404 Not Found'
+        return '200 OK\n'
 
 
 run(host='localhost', port=8080, debug=True)
